e68e752ee9b903d58cf2eb123812ac67692194cc
Rakia tehreen <138242377+rakiatehreen10@users.noreply.github.com>
2026-01-30 10:51:01 +0530

    updated splash screen

--- src/screens/auth/splashscreen/SplashScreen.jsx
+++ src/screens/auth/splashscreen/SplashScreen.jsx
@@ -1,13 +1,64 @@
-import { View, Text, TouchableOpacity } from 'react-native'
-import React from 'react'
-import CustomButton from "../../../components/customButton/CustomButton"
+import { View, Image, ImageBackground, StyleSheet, ActivityIndicator, Dimensions } from 'react-native';
 
+import React, { useEffect } from 'react';
+import { useNavigation } from '@react-navigation/native';
+
+import back from '../../../assest/back.png';
+import mid from '../../../assest/mid.png';
+
+const { width, height } = Dimensions.get('window');
+
+ 
 const SplashScreen = () => {
-    return (
-        <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+     const navigation = useNavigation(); 
 
-        </View>
-    )
-}
+  useEffect(() => {
+     // 3 sec after
+    const timer = setTimeout(() => {
+      navigation.replace('OnBoarding'); 
+    }, 300000);
 
-export default SplashScreen
+    return () => clearTimeout(timer); 
+  }, []);
+
+
+  return (
+    
+    <ImageBackground 
+      source={back} 
+      style={styles.backg} 
+      resizeMode="cover"        
+    >
+      <Image source={mid} style={styles.mid} />
+      
+      <ActivityIndicator
+        size="large"
+        color="#0A5ED7"
+        style={styles.loader}
+      />
+    </ImageBackground>
+  );
+};
+
+export default SplashScreen;
+
+const styles = StyleSheet.create({
+  backg: {
+    flex: 1,
+    width: width,
+    height: height,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  mid: {
+    width: 230,
+    height: 172,
+    resizeMode: 'cover',
+  },
+
+  loader: {
+    position: 'absolute',
+    bottom: height * 0.25, // for responsive  
+  },
+});
